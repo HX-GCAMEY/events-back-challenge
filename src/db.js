a1c6ba082d7152135e556165aef27acbee3947fb
Gama <lic.camey@gmail.com>
2023-08-30 10:42:30 -0300

    prueba

--- src/db.js
+++ src/db.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const {DB_USER, DB_PASSWORD} = process.env;
 
+console.log("Si pude mama");
+
 async function main() {
   await mongoose.connect(
     `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.cfkr92h.mongodb.net/`
